e4eb882fca84567e210d0ff514b9e3cd2d4391d7
nakatsu746 <jilptfes746@gmail.com>
2023-11-12 19:52:16 +0900

    ラインが揃っているか確認する処理が抜けていたため修正

--- src/entity/field.ts
+++ src/entity/field.ts
@@ -90,6 +90,8 @@ export class Field {
         // 斜め
         if (this.checkLine([this.field[0][0], this.field[1][1], this.field[2][2]]) !== MarkType.None) {
             return this.field[0][0];
+        } else if (this.checkLine([this.field[0][2], this.field[1][1], this.field[2][0]]) !== MarkType.None) {
+            return this.field[0][2];
         }
         return MarkType.None;
     }
